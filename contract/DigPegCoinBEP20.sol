708228fcc43201114b24ba976d7c6e2179a50652
katerega <8218kelmo@gmail.com>
2024-06-04 05:39:35 +0300

    Update DigPegCoinBEP20.sol

--- contract/DigPegCoinBEP20.sol
+++ contract/DigPegCoinBEP20.sol
@@ -4,9 +4,22 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract DigiPegCoin is ERC20, Ownable {
-    uint256 public constant INITIAL_SUPPLY = 100_000_000 * (10 ** 18); // 100 million tokens with 18 decimals
+interface IBEP20 {
+    function totalSupply() external view returns (uint256);
+    function decimals() external view returns (uint8);
+    function symbol() external view returns (string memory);
+    function name() external view returns (string memory);
+    function balanceOf(address account) external view returns (uint256);
+    function transfer(address recipient, uint256 amount) external returns (bool);
+    function allowance(address owner, address spender) external view returns (uint256);
+    function approve(address spender, uint256 amount) external returns (bool);
+    function transferFrom(address sender, address recipient, uint256 amount) external returns (bool);
 
+    event Transfer(address indexed from, address indexed to, uint256 value);
+    event Approval(address indexed owner, address indexed spender, uint256 value);
+}
+
+contract DigiPegCoin is ERC20, Ownable {
     address public presaleAddress;
     address public liquidityPoolAddress;
     address public marketingAddress;
@@ -18,13 +31,16 @@ contract DigiPegCoin is ERC20, Ownable {
         address _liquidityPoolAddress,
         address _marketingAddress,
         address _teamAddress,
-        address _communityRewardsAddress
-    ) ERC20("DigiPegCoin", "DPC") {
+        address _communityRewardsAddress,
+        address _initialOwner // Pass initial owner address here
+    ) ERC20("DigiPegCoin", "DPC") Ownable(_initialOwner) {
         presaleAddress = _presaleAddress;
         liquidityPoolAddress = _liquidityPoolAddress;
         marketingAddress = _marketingAddress;
         teamAddress = _teamAddress;
         communityRewardsAddress = _communityRewardsAddress;
+
+        uint256 INITIAL_SUPPLY = 100_000_000 * (10 ** uint256(decimals())); // 100 million tokens with 18 decimals
 
         _mint(msg.sender, INITIAL_SUPPLY * 5 / 100); // Owner gets 5%
         _mint(presaleAddress, INITIAL_SUPPLY * 60 / 100); // 60% for presale
@@ -33,13 +49,4 @@ contract DigiPegCoin is ERC20, Ownable {
         _mint(teamAddress, INITIAL_SUPPLY * 10 / 100); // 10% for team and development
         _mint(communityRewardsAddress, INITIAL_SUPPLY * 5 / 100); // 5% for community rewards
     }
-
-    function mint(address to, uint256 amount) public onlyOwner {
-        _mint(to, amount);
-    }
-
-    function burn(uint256 amount) public {
-        _burn(msg.sender, amount);
-    }
 }
-
